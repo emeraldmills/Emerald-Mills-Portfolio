159454667e3dc111e6dfaecaba59a408f82b6b27
emeraldmills <emeraldmillsxo@gmail.com>
2026-03-23 19:50:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-// Turn.js flipbook setup
 $(function () {
   const $flip = $("#flipbook");
 
-  // If Turn.js didn't load, this prevents a silent "nothing happens"
+  // If Turn.js didn’t load, stop here (prevents “it shows code” / broken)
   if (typeof $flip.turn !== "function") {
-    console.error("Turn.js did not load. Check the script link in index.html.");
+    alert("Turn.js did not load. Check your script links and try refresh (Cmd+Shift+R).");
     return;
   }
 
+  function sizeBook() {
+    const wrap = document.querySelector(".bookWrap");
+    const w = wrap.clientWidth;
+    const h = wrap.clientHeight;
+
+    // double-page book width should be close to wrap width
+    $flip.turn("size", w, h);
+  }
+
+  // Init turn.js
   $flip.turn({
-    width: $flip.parent().width(),
-    height: 560,
-    autoCenter: true,
+    width: document.querySelector(".bookWrap").clientWidth,
+    height: document.querySelector(".bookWrap").clientHeight,
+
     display: "double",
+    autoCenter: true,
+
+    // This is the “speed” (ms). Higher = slower.
+    duration: 1100,
+
+    // These two make it feel more like paper
     acceleration: true,
-    gradients: true
+    gradients: true,
+
+    // Makes page edges feel thicker / more realistic
+    elevation: 50
   });
 
   // Buttons
@@ -26,10 +44,11 @@ $(function () {
     $flip.turn("previous");
   });
 
-  // Resize responsiveness
+  // Resize handler
   $(window).on("resize", function () {
-    const w = $flip.parent().width();
-    $flip.turn("size", w, 560);
-    $flip.turn("center");
+    sizeBook();
   });
+
+  // First size
+  sizeBook();
 });
